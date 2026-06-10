6e35a3618f41c3c3171a30c8277f2a8c7ba41688
Yuvasri-Zuci <yuvasri.gurusamy@zucisystems.com>
2026-06-10 12:22:26 +0530

    pages created for upcoming module

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UpComp } from './up-comp/up-comp';
+import { Match } from './match/match';
+
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [RouterOutlet],
+  standalone:true,
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

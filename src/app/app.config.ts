ff03ce8494306ed49bb4cd5765dca008a21e1e39
Yuvasri-Zuci <yuvasri.gurusamy@zucisystems.com>
2026-06-11 22:38:44 +0530

    navBar

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,13 +6,5 @@ import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-<<<<<<< HEAD
   providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes), provideHttpClient()],
-=======
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
-    provideHttpClient()
-  ]
->>>>>>> 06811aad4e483b47e0dd5b292826b83c62abf6aa
 };

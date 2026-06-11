15fdd88a588c7bd2222a6578200904d21539b0eb
Yuvasri-Zuci <yuvasri.gurusamy@zucisystems.com>
2026-06-11 19:08:36 +0530

    developer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Squads } from './squads/squads';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'live',
+    pathMatch: 'full',
+  },
+  {
+    path: 'squads',
+    component: Squads,
+  },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Squads } from './squads/squads';
+import { Nav } from './nav/nav';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Squads, RouterOutlet, Nav],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  standalone: true,
 })
 export class App {
   protected readonly title = signal('cricinfo-clone');

74ddbe0017d02e57ea9922fdd6b27136c89c98bf
SelvarathinamKaruppusamy <selva.karuppusamy@zucisystems.com>
2026-06-22 21:26:40 +0530

    Add Admin folder

---
+++ src/app/Admin/live-admin/live-admin.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-live-admin',
+  imports: [],
+  templateUrl: './live-admin.html',
+  styleUrl: './live-admin.css',
+})
+export class LiveAdmin {}
